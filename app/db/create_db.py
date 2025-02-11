ec85df97576650db514dc484832fc94dbcb33dbe
facha <13649860+franhace@users.noreply.github.com>
2025-02-10 23:14:41 -0300

    add - createdb script

---
+++ app/db/create_db.py
@@ -0,0 +1,22 @@
+import os
+
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from app.models.base import Base
+# models get registered with the Base metadata
+from app.models.job import Job
+from app.models.company import Company
+from app.models.country import Country
+
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
+def create_tables():
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+    Base.metadata.create_all(bind=engine)
+    print("Tables created successfully.")
+
+if __name__ == "__main__":
+    # pass
+    create_tables()
