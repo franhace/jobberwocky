28010621b040a9c35038c63ea519ab2d7d7b6633
facha <13649860+franhace@users.noreply.github.com>
2025-02-10 21:11:48 -0300

    mod - separate Job model, add Company and add Country models

--- app/models/job.py
+++ app/models/job.py
@@ -1,12 +1,21 @@
-from pydantic import BaseModel
+from typing import List
 
-class JobCreate(BaseModel):
-    title: str
-    description: str
-    company: str
+from pydantic import BaseModel, Field
+from sqlalchemy import Column, Integer, String, ForeignKey, JSON
+from sqlalchemy.orm import relationship
 
-class Job(JobCreate):
-    id: int
 
-    class Config:
-        orm_mode = True
+class Job(BaseModel):
+    __tablename__ = "jobs"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    description = Column(String)
+    salary = Column(Integer, nullable=False)
+    skills = Column(JSON, default=[])
+
+    company_id = Column(Integer, ForeignKey("companies.id"), nullable=False)
+    country_id = Column(Integer, ForeignKey("countries.id"), nullable=False)
+
+    company = relationship("Company", back_populates="jobs")
+    country = relationship("Country", back_populates="jobs")
